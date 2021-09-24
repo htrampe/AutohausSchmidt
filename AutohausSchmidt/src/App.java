adb6fa8a894b220cffc13a55ff2b7e919b968ddf
Holger Trampe <htrampe@gmail.com>
2021-09-24 09:30:08 +0200

    HA usw.

--- AutohausSchmidt/src/App.java
+++ AutohausSchmidt/src/App.java
@@ -1,12 +1,61 @@
 public class App {
-    /**
-     * 
-     * @param args
-     * @throws Exception
-     */
+
+    static Auto[] _autos;
+    static Kunde[] _kunden;
+
+    static Verkauf[] _verkaeufe;
 
     public static void main(String[] args) throws Exception {
+        //Initialisieren von _autos
+        _autos = new Auto[8];
+
+        _verkaeufe = new Verkauf[8];
+
+        _autos[0] = new Auto("VW", "Polo");
+        _autos[1] = new Auto("Toyota", "Corolla");
+        _autos[2] = new Auto("VW", "Touran");
+
         Menu menu = new Menu();
         menu.startMenu();
+    }
+
+    
+    
+    public static void addAuto(Object auto){
+        int i = 0;
+        for(Auto a : getAllAutos()){
+            if(a == null){
+                _autos[i] = (Auto) auto;
+                break;
+            }
+            i = i + 1;
+
+            if(i > 7){
+                System.out.println("Kein Platz mehr auf dem Hof vom Schmidt...");
+            }
+        }
+    }   
+
+    public static void addObject(Object ele, Object[] array){
+        int i = 0;
+        for(Object array_ele : array){
+            if(array_ele == null){
+                array[i] = ele;
+                break;
+            }
+            else{
+                i++;
+            }
+        }
+    }
+    
+    
+    
+    public static Auto[] getAllAutos(){
+        return _autos;
+    }
+
+    public static Kunde[] getAllKunden(){
+        return _kunden;
     }
 }
--- AutohausSchmidt/src/Menu.java
+++ AutohausSchmidt/src/Menu.java
@@ -14,14 +14,17 @@ public class Menu {
         System.out.println("(2) Neuer Kunde");
         System.out.println("(3) Neues Auto");
         System.out.println("(4) Neuer Verkäufer");
+        System.out.println("(5) Zeige alle Autos");
+        System.out.println("(6) Zeige alle Kunden");
+        System.out.println("(7) Zeige alle Verkäufer");
         String choice = getScanner().nextLine();
         System.out.println("Deine Auswahl ist: " + choice);
 
         if(choice.equals("1")){
-            System.out.println("Verkauf noch nicht implementiert...");
+            createVerkaeufeMenu();
         }
         else if(choice.equals("2")){
-            System.out.println("Neuer Kunde noch nicht implementiert...");
+            createKundeMenu();
         }
         else if(choice.equals("3")){
             createAutoMenu();
@@ -29,11 +32,66 @@ public class Menu {
         else if(choice.equals("4")){
             System.out.println("Neuer Verkäufer noch nicht implementiert...");
         }
+        else if(choice.equals("5")){
+            showAutos();
+        }
+        else if(choice.equals("6")){
+            showKunden();
+        }
+        else if(choice.equals("7")){
+            showVerkaeufer();
+        }
         else {
-            System.out.println("Bitte nur 1,2,3,4 eingeben!");
+            System.out.println("Bitte nur 1,2,3,4,5,6 eingeben!");
         }
         startMenu();
     }
+
+    public void showAutos(){
+        for(Auto auto : App.getAllAutos()) {
+            if(auto != null){
+                System.out.println(auto.getMarke() + " " + auto.getModell());
+            }
+        }
+    }
+
+    public void showKunden(){
+        for(Kunde kunde : App.getAllKunden()) {
+            if(kunde != null){
+                System.out.println();
+            }
+        }
+    }
+
+    public void showVerkaeufer(){
+        for(Kunde kunde : App.getAllKunden()) {
+            if(kunde != null){
+                System.out.println();
+            }
+        }
+    }
+
+    public void createVerkaeufeMenu(){
+        int i = 0;
+        System.out.println("Bitte wählen Sie ein Auto aus:");
+        for(Auto auto : App.getAllAutos()){
+            if(auto != null){
+                System.out.println(i + " - " + auto.getMarke() + " " + auto.getModell());
+                i++;
+            }
+        }
+        String car_choice = getScanner().next();
+        int car_index = Integer.valueOf(car_choice);
+
+        System.out.println(App.getAllAutos()[car_index]);
+
+        // Verkauf v = new Verkauf(App.getAllAutos()[car_index]);
+        // Hausaufgabe: Das alles auf Verkäufer und Kunde übertragen, einen Verkauf final erstellen und einen Menüpunkt erstellen, um alle Verkäufe auszugeben :) ! Achtung: Bei Verkauf muss auch ein Int umgewandelt werden, damit wir einen Preis speichern können!
+    }
+
+
+
+
 
     
     public void createAutoMenu(){
@@ -42,7 +100,24 @@ public class Menu {
         String marke = this.getScanner().nextLine();
         System.out.print("Modell: ");
         String modell = this.getScanner().nextLine();
-        Auto a1 = new Auto(marke, modell);
+        
+        App.addAuto(new Auto(marke, modell));
+        
+        System.out.println(App.getAllAutos());
+
+        startMenu();
+    }
+
+    public void createKundeMenu(){
+        System.out.println("Bitte Daten des neuen Kunden eingeben:");
+        System.out.print("Vorname: ");
+        String vorname = this.getScanner().nextLine();
+        System.out.print("Nachname: ");
+        String nachname = this.getScanner().nextLine();
+        Kunde k = new Kunde(vorname, nachname);
+
+        System.out.println("Der neue Kunde heißt " + k.getVorname() + " " + k.getNachname());
+
         startMenu();
     }
 
